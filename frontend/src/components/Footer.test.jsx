5ae9a78025244d5fb2e5fad5f319e5fbbee8f55c
Subham Gope <subhamgopebusiness@gmail.com>
2025-07-08 21:11:14 +0530

    vercel production deploy solved

--- frontend/src/components/Footer.test.jsx
+++ frontend/src/components/Footer.test.jsx
@@ -1,9 +1,18 @@
 import { describe, expect, test } from "vitest";
 import { screen, render } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import Footer from "./Footer";
 
 describe("Testing the Footer.jsx file", () => {
   test("renders logo, title, quick link and contact information", () => {
+    //  rending with MemoryRouter for navigation
+    const { container } = render(
+      <MemoryRouter>
+        <Footer />
+      </MemoryRouter>
+    );
 
-    
+    // snapshot matching
+    expect(container).toMatchSnapshot();
   });
 });
