867537b9601bd28ea50e4facff30cd60d540e626
Subham Gope <subhamgopebusiness@gmail.com>
2025-07-17 12:49:28 +0530

    The Title.jsx file testing is completed

---
+++ frontend/src/components/Title.test.jsx
@@ -0,0 +1,17 @@
+import { describe, test, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import Title from "./Title";
+
+describe("Testing the Title.jsx file", () => {
+  test("render the title", () => {
+    // Rendering and setting up the snapshot matching
+    const { container } = render(<Title text1={`Hello`} text2={`There`} />);
+    expect(container).toMatchSnapshot();
+
+    // checking for the texts
+    const text1 = screen.getByText(/hello/i);
+    expect(text1).toBeInTheDocument();
+    const text2 = screen.getByText(/there/i);
+    expect(text2).toBeInTheDocument();
+  });
+});
